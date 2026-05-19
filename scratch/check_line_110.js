12e5413c31ad5e286f07361f896da5fa02e9d87c
proknicis <proknicis@gmail.com>
2026-05-19 20:47:07 +0300

    Cleanup unused debug and temporary files

--- scratch/check_line_110.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('c:\\Users\\prokn\\Desktop\\blonk\\src\\app\\dashboard\\page.module.css', 'utf8');
-const lines = content.split('\n');
-const line110 = lines[109]; // 0-indexed
-console.log(`Line 110: "${line110}"`);
-for (let i = 0; i < line110.length; i++) {
-    console.log(`${i}: ${line110[i]} (${line110.charCodeAt(i)})`);
-}
--- scratch/dump_ops.js
+++
@@ -1,8 +0,0 @@
-const { Pool } = require('pg'); 
-require('dotenv').config(); 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL }); 
-async function run() { 
-    console.log("OperationalSetting:", (await pool.query(`SELECT * FROM "OperationalSetting"`)).rows);
-    await pool.end(); 
-} 
-run().catch(console.dir);
